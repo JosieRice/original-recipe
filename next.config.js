49821ecf4cc1dd97265088b8b32de7c9cc5ec625
JosieRice <josiericetong@gmail.com>
2023-10-04 15:16:59 -0700

    added a recipe card component

--- next.config.js
+++ next.config.js
@@ -5,12 +5,20 @@ const nextConfig = {
     },
     images: {
         remotePatterns: [
+          // for gmail login avatar picture
           {
             hostname: 'lh3.googleusercontent.com',
             pathname: '**',
             port: '',
             protocol: 'https',
           },
+          // for temporary placeholder image
+          {
+            hostname: 'cdn.freecodecamp.org',
+            pathname: '**',
+            port: '',
+            protocol: 'https'
+          }
         ],
       },
 };
